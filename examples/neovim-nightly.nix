bad70b7980f25bb6d1cfa3c204b66d914f771735
Yusuke Shibata <yusukeshib@gmail.com>
2026-01-24 11:41:31 -0500

    Remove example

--- examples/neovim-nightly.nix
+++
@@ -1,8 +0,0 @@
-{
-  name = "neovim-nightly";
-  inputs = {
-    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
-  };
-  overlay = "neovim-nightly-overlay.overlays.default";
-  packageExpr = "pkgs.neovim";
-}
